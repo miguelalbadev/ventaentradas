ef2ce55ad04cad15cadb75767c29738f539822c3
java <miguelalbadev@gmail.com>
2017-08-27 01:31:41 +0200

    html y 2 componentes maestros completos

---
+++ src/components/MaestroEntradas.vue
@@ -0,0 +1,21 @@
+<template>
+  
+    <h2>Lo que el viento se llevó</h2>
+  
+</template>
+
+<script>
+//import Hello from './components/Hello'
+
+export default {
+  name: 'maestroentradas'
+  
+}
+</script>
+
+<style>
+#h2 {
+  font-family: 'Verdana', Helvetica, Arial, sans-serif;
+ 
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,47 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+//import MaestroEntradas from './components/MaestroEntradas'
 
-Vue.config.productionTip = false
 
-/* eslint-disable no-new */
+var maestroEntradas = Vue.component('entradas',{
+	  template: `
+        <ol>
+          <li v-for="entrada in entradasList">
+          	<a @href.prevent="" v-on:click="">
+            {{entrada}}
+            </a>
+          </li>
+        </ol>
+    `,
+
+	  data:function(){
+	  	return {entradasList: ["Entrada 1","Entrada 2","Entrada 3"]}
+	  }
+})
+
+var peliculas = Vue.component('peliculas',{
+	template: `
+        <ol>
+          <li v-for="pelicula in peliculasList">
+          <a @href.prevent="" v-on:click="">
+            {{pelicula}}
+           </a>
+          </li>
+        </ol>
+    `,
+    data:function() {
+    return {peliculasList: ["El Padrino","Casablanca","El cabo del miedo"]}
+  }
+})
+
 new Vue({
-  el: '#app',
-  template: '<App/>',
-  components: { App }
+	el: '#tab-1'
+	
+		
+})
+
+new Vue({
+	el:'#tab-2'
+	
 })
